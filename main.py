c2c84363bd6b1abaa70e37a4bb47e14320aa4cb8
Max-Black-De <black_de@yahoo.com>
2025-03-24 23:38:56 +0500

    Фиксит бак с воспроизведением аудио V-1.0.1

--- main.py
+++ main.py
@@ -1,22 +1,8 @@
-import kivy
 from kivy.app import App
-from kivy.uix.screenmanager import ScreenManager, Screen
-from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager
 from screens.audio_list_screen import AudioListScreen
 from screens.create_audio_screen import CreateAudioScreen
-
-# стартовый
-class WelcomeScreen(Screen):
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        layout = BoxLayout(orientation='vertical')
-        button = Button(text='Продолжить', on_press=self.goto_input)
-        layout.add_widget(button)
-        self.add_widget(layout)
-
-    def goto_input(self, instance):
-        self.manager.current = 'input_screen'
+from screens.welcome_screen import WelcomeScreen
 
 class AudioApp(App):
     def build(self):
